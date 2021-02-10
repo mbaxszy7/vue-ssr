da8e3c0b84ef8410a78c5ddd3442550c14a4c5f1
Zheyu Yan <390016746@qq.com>
2021-02-10 18:00:02 +0800

    change webpack dev middle version

--- build/setup-dev-server.js
+++ build/setup-dev-server.js
@@ -4,12 +4,15 @@ const chokidar = require("chokidar")
 const webpack = require("webpack")
 const webpackMiddleware = require("webpack-dev-middleware")
 
+const webpackServerConfig = require("./webpack.server.config.js")
+
 const resolve = (file) => path.resolve(__dirname, file)
 
 module.exports = (server, callback) => {
   let ready
-  // eslint-disable-next-line no-return-assign
-  const onReady = new Promise((r) => (ready = r))
+  const onReady = new Promise((r) => {
+    ready = r
+  })
   // 构建
   let serverBundle
   let template
@@ -31,20 +34,19 @@ module.exports = (server, callback) => {
   })
 
   // 监视构建serverBundle -> 调用update
-  // eslint-disable-next-line global-require
-  const serverConfig = require("./webpack.server.config.js")
-  const serverCompiler = webpack(serverConfig)
-  webpackMiddleware(serverCompiler, {
-    publicPath: serverConfig.output.publicPathoutput,
+  const serverCompiler = webpack(webpackServerConfig)
+  const serverDevMiddle = webpackMiddleware(serverCompiler, {
+    logLevel: "silent",
   })
-  // serverCompiler.watch({}, (err, stats) => {
-  //   if (err) throw err;
-  //   if (stats.hasErrors()) return;
-  //   serverBundle = JSON.parse(
-  //     fs.readFileSync(resolve("../dist/vue-ssr-server-bundle.json"), "utf-8")
-  //   );
-  //   update();
-  // });
+  serverCompiler.hooks.done.tap("server", () => {
+    serverBundle = JSON.parse(
+      serverDevMiddle.fileSystem.readFileSync(
+        resolve("../dist/vue-ssr-server-bundle.json"),
+        "utf-8",
+      ),
+    )
+    update()
+  })
 
   // 监视构建clientManifest -> 调用update
 
